792b2207adf41cc430fc9e899e3f939433bc2f1e
Balos, Cody, J <balos1@llnl.gov>
2023-01-10 17:14:19 -0800

    tighten tolerances for cvRoberts_dns_constraints when extended precision

--- examples/cvodes/serial/cvsRoberts_FSA_dns_constraints.c
+++ examples/cvodes/serial/cvsRoberts_FSA_dns_constraints.c
@@ -87,10 +87,17 @@
 #define Y1    RCONST(1.0)      /* initial y components */
 #define Y2    RCONST(0.0)
 #define Y3    RCONST(0.0)
+#if defined(SUNDIALS_EXTENDED_PRECISION)
+#define RTOL  RCONST(1.0e-6)   /* scalar relative tolerance            */
+#define ATOL1 RCONST(1.0e-8)   /* vector absolute tolerance components */
+#define ATOL2 RCONST(1.0e-13)
+#define ATOL3 RCONST(1.0e-7)
+#else
 #define RTOL  RCONST(1.0e-4)   /* scalar relative tolerance            */
 #define ATOL1 RCONST(1.0e-6)   /* vector absolute tolerance components */
 #define ATOL2 RCONST(1.0e-11)
 #define ATOL3 RCONST(1.0e-5)
+#endif
 #define T0    RCONST(0.0)      /* initial time           */
 #define T1    RCONST(0.4)      /* first output time      */
 #define TMULT RCONST(10.0)     /* output time factor     */
